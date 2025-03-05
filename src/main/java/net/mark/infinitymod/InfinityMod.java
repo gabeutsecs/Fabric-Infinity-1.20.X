6f670d249862a2aba794019401cf9ca37c501800
gabeutsecs <metamodernjew@gmail.com>
2025-03-05 18:37:00 +0100

    change

---
+++ src/main/java/net/mark/infinitymod/InfinityMod.java
@@ -0,0 +1,15 @@
+package net.mark.infinitymod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class InfinityMod implements ModInitializer {
+	public static final String MOD_ID = "infinitymod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
