b5a2d35046ceaf25cd5ca2beafdb129f5a99f66c
gabeutsecs <metamodernjew@gmail.com>
2025-03-05 18:38:37 +0100

    change

---
+++ src/main/java/net/mark/infinitymod/InfinityModClient.java
@@ -0,0 +1,10 @@
+package net.mark.infinitymod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class InfinityModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
